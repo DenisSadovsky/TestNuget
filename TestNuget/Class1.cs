79e5bb2b95e56d6922551631e9815be2476c18f4
Dzianis Sadouski <Dzianis_Sadouski@epam.com>
2020-03-04 15:56:35 +0300

    Add project files.

---
+++ TestNuget/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNuget
+{
+    public class MathOperation
+    {
+        public static int Summ(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
